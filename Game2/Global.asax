927d2ade46f806bd7a62717bad1572a325ef23c2
herruber <asteroth69@hotmail.com>
2017-12-17 16:22:26 +0100

    Add project files.

---
+++ Game2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Game2.MvcApplication" Language="C#" %>
---
+++ Game2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Game2.Startup))]
+namespace Game2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
